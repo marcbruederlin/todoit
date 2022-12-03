7b035c10934508475e95afca55d1a1ae86389008
Marc Brüderlin <marc.bruederlin@exconcept.com>
2022-12-03 16:01:01 +0100

    Improve checkbox UX

--- src/components/common/Checkbox.tsx
+++ src/components/common/Checkbox.tsx
@@ -3,7 +3,7 @@ export default function Checkbox({ isChecked, onChange }: Props) {
     <input
       type="checkbox"
       checked={isChecked}
-      className="h-5 w-5 appearance-none rounded-sm border border-gray-300 shadow-sm checked:border-indigo-600 checked:bg-indigo-600 checked:bg-check-icon"
+      className="h-5 w-5 cursor-pointer appearance-none rounded-sm border border-gray-300 shadow-sm transition-all checked:border-indigo-600 checked:bg-indigo-600 checked:bg-check-icon hover:border-indigo-600"
       onChange={() => onChange()}
     />
   )
